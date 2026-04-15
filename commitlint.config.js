c88401557ddef83bb980517ef87c7a410b5e441b
Wojciech Piątkowski <chmurson@gmail.com>
2026-04-15 15:02:29 +0200

    chore: add more explicit error message for commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,24 @@
+const types = [
+  'feat',     // New feature
+  'fix',      // Bug fix
+  'docs',     // Documentation changes
+  'style',    // Code formatting (no code change)
+  'refactor', // Code refactoring
+  'perf',     // Performance improvement
+  'test',     // Adding or fixing tests
+  'build',    // Build system changes
+  'ci',       // CI configuration
+  'chore',    // Other changes
+];
+
 export default {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      types,
+    ],
+  },
+  helpUrl: `https://github.com/conventional-changelog/commitlint/#what-is-commitlint;\n    Required types: ${types.join(', ')}`,
 };
